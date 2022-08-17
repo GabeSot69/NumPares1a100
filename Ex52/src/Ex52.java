488bef0e3746ff6a82da2fd0ee777cf4b518d799
GabeSot69 <105126836+GabeSot69@users.noreply.github.com>
2022-08-16 21:48:44 -0400

    Add files via upload

---
+++ Ex52/src/Ex52.java
@@ -0,0 +1,17 @@
+/*
+	Nome: Gabriel Sotero Mendonça
+	Objetivo: Faça um programa que imprima todos os números pares no intervalo de 1 a 100.
+ */
+public class Ex52 {
+	public static void main(String[] args) {
+		System.out.println("Numeros pares de 1 a 100");
+		for (int i = 1; i <= 100; i++) {
+			if (i % 2 == 0) {
+				System.out.println(i);
+			}
+			
+			
+		}
+	}
+
+}
